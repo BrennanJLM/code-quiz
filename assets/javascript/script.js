303fd2108f40893c370c4ed32e4fb2422d603327
Brennan <merrett.b@gmail.com>
2022-03-24 22:15:16 -0400

    added questions in Javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,133 +1,135 @@
-//view high scores link in top right of page
-var timeleft = 75;
-var downloadTimer = setInterval(function(){
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    document.getElementById("countdown-timer").innerHTML = "Finished";
-  } else {
-    document.getElementById("countdown-timer").innerHTML = timeleft + " seconds remaining";
-  }
-  timeleft -= 1;
+var startButton = document.getElementById("start-btn")
+var nextButton = document.getElementById("next-btn")
+var questionContainerElement = document.getElementById("question-container")
+var questionElement = document.getElementById("question")
+var answerButtonsElement = document.getElementById("answer-buttons")
+var wrongAnswers = document.getElementById("wrong-answers")
+
+var shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener("click", startQuiz)
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+function startQuiz() {
+  startButton.classList.add("hide")
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove("hide")
+  setNextQuestion()
+}
+
+function setNextQuestion() {
+    resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    var selectedButton = e.target
+    var correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide")
+}   else {
+    startButton.innerText = "Restart"
+    startButton.classList.remove ("hide")
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if(correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
+}}
+
+var start = Date.now();
+setInterval(function() {
+    var delta = Date.now() - 75000; 
+    var delta = Date.end() - 0;
+    output (Math.floor(delta / 1000)); 
+    output (new Date() .toUTCString());
 }, 1000);
 
-//time limit for quiz posted in top right of page. 75 seconds.
 
-//1st question
-var questions= [
+var questions = [
     {
-        question :'commonly used data types DO not include:',
-        choiceA:  'strings',
-        choiceB: 'booleans',
-        choiceC: 'alerts',
-        choiceD: 'numbers',
-        answer: 'alerts'
-
+        question: "Commonly used data types DO NOT include:",
+        answers: [
+            { text: "strings", correct: false},
+            { text: "booleans", correct: false},
+            { text: "numbers", correct: false},
+            { text: "alerts", correct: true}
+        ]
     },
     {
-        question :'The condition of an if/else statement is enlcosed with      . ',
-        choiceA: 'quotes',
-        choiceB: 'curly brackets',
-        choiceC: 'parenthesis',
-        choiceD: 'square brackets',
-        answer: 'parenthesis'
-
+        question: "The condition of an if/else statement is enlcosed with     .:",
+        answers: [
+            { text: "quotes", correct: false},
+            { text: "curly brackets", correct: false},
+            { text: "parenthesis", correct: true},
+            { text: "square brackets", correct: false}
+        ]
     },
     {
-        question :'Arrays in Javascript can be used to store:',
-        choiceA: 'numbers and strings',
-        choiceB: 'other arrays',
-        choiceC: 'alerts',
-        choiceD: 'all of the above',
-        answer: 'all of the above'
-
+        question: "Arrays in Javascript can be used to store:",
+        answers: [
+            { text: "numbers and strings", correct: false},
+            { text: "other arrays", correct: false},
+            { text: "alerts", correct: false},
+            { text: "all of the above", correct: true}
+        ]
     },
     {
-        question :'String values must be enclosed within        when being assigned to variables',
-        choiceA: 'commas',
-        choiceB: 'curly brackets',
-        choiceC: 'booleans',
-        choiceD: 'quotes',
-        answer: 'parenthesis'
-
+        question: "String values must be enclosed within        when being assigned to variables:",
+        answers: [
+            { text: "commas", correct: false},
+            { text: "curly brackets", correct: false},
+            { text: "booleans", correct: false},
+            { text: "quotes", correct: true}
+        ]
     },
     {
-        question :'A very useful tool used during development and debugging for printing content to the debugger is:',
-        choiceA: 'Javascript',
-        choiceB: 'terminal/bash',
-        choiceC: 'alerts',
-        choiceD: 'console.log',
-        answer: 'console.log'
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: [
+            { text: "Javascript", correct: false},
+            { text: "terminal/bash", correct: false},
+            { text: "alerts", correct: false},
+            { text: "console.log", correct: true}
+        ]
+    },
 
-    }
-
-];
-
-
-console.log(questions);
-// commonly used data types DO not include:
-// 1.strings
-// 2.booleans
-// 3.alerts
-// 4.numbers
-
-//alert whether wrong or correct
-//if wrong subtract 10 seconds from timer
-
-//2nd question
-
-// The condition of an if/else statement is enlcosed with      . 
-// 1.quotes
-// 2.curly brackets
-// 3.parenthesis
-// 4.square brackets
-
-//alert whether wrong or correct
-//if wrong subtract 10 seconds from timer
-
-//3rd question
-
-// Arrays in Javascript can be used to store:
-// 1.numbers and strings
-// 2.other arrays
-// 3.booleans
-// 4.all of the above
-
-//alert whether wrong or correct
-//if wrong subtract 10 seconds from timer
-
-//4th question
-
-// String values must be enclosed within        when being
-// assigned to variables.
-// 1.commas
-// 2.curly brackets
-// 3.quotes
-// 4.parenthesis
-
-//alert whether wrong or correct
-//if wrong subtract 10 seconds from timer
-
-//5th question
-
-// A very useful tool used during development and debugging 
-// for printing content to the debugger is:
-// 1.Javascript
-// 2.terminal/bash
-// 3.for loops
-// 4.console.log
-
-//alert whether wrong or correct
-//if wrong subtract 10 seconds from timer
-
-//score/enter intitals screen
-
-// All Done!
-// your final score is :
-// enter initials :
-// submit button
-
-//end screen
-
-// High Scores:
-// Go Back button
-// Clear High Scores button
+]
